da84bb6c9b77ae817ddec7cc22afe4474141b693
Lithius <aerosiiap@gmail.com>
2021-09-24 12:09:42 -0400

    Renamed accuracy to be more descriptive
    Using "Accuracy" makes no sense considering a high "Accuracy" mean a less accurate shot

--- src/main/java/com/flansmod/common/guns/ItemGun.java
+++ src/main/java/com/flansmod/common/guns/ItemGun.java
@@ -1071,7 +1071,7 @@ public class ItemGun extends Item implements IPaintableItem
 		if(type.showRecoil)
 			lines.add("\u00a79Recoil" + "\u00a77: " + type.getRecoil(stack));
 		if(type.showSpread)
-			lines.add("\u00a79Accuracy" + "\u00a77: " + type.getSpread(stack));
+			lines.add("\u00a79Spread" + "\u00a77: " + type.getSpread(stack));
 		if(type.showReloadTime)
 			lines.add("\u00a79Reload Time" + "\u00a77: " + type.getReloadTime(stack) / 20 + "s");
 		for(AttachmentType attachment : type.getCurrentAttachments(stack))
