8dc8374c475b857a48b1dc66c4e8a057e2c33951
Lithius <aerosiiap@gmail.com>
2021-12-14 15:49:03 -0500

    Reworked camera
    Added constructor to allow custom camera offsets for each vehicle and refactored some of the code.

--- src/main/java/com/flansmod/client/EntityCamera.java
+++ src/main/java/com/flansmod/client/EntityCamera.java
@@ -1,6 +1,7 @@
 package com.flansmod.client;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHandSide;
@@ -9,10 +10,16 @@ import net.minecraft.world.World;
 import com.flansmod.common.driveables.EntityDriveable;
 import com.flansmod.common.vector.Vector3f;
 
+/**
+ * Third-person camera for driveables. 
+ * 
+ * Applies a rotated offset from the driveable position.
+ * Camera has a linear interpolation step to smooth motion.
+ */
 public class EntityCamera extends EntityLivingBase
 {
 	public EntityDriveable driveable;
-	
+	public Vector3f offset;
 	public EntityCamera(World world)
 	{
 		super(world);
@@ -21,9 +28,15 @@ public class EntityCamera extends EntityLivingBase
 	
 	public EntityCamera(World world, EntityDriveable d)
 	{
+		this(world, d, new Vector3f(-10f, 2f, 0f));
+	}
+	
+	public EntityCamera(World world, EntityDriveable d, Vector3f offset)
+	{
 		this(world);
 		driveable = d;
 		setPosition(d.posX, d.posY, d.posZ);
+		this.offset = offset; //TODO: Make this a vehicle config later
 	}
 	
 	@Override
@@ -33,21 +46,31 @@ public class EntityCamera extends EntityLivingBase
 		prevPosY = posY;
 		prevPosZ = posZ;
 		
-		Vector3f cameraPosition = new Vector3f();
-		cameraPosition = driveable.axes.findLocalVectorGlobally(cameraPosition);
+		//Finding the position the camera is moving towards
+		//This is just the driveable's position with the rotated applied offset.
+		Vector3f desiredPosition = driveable.axes.findLocalVectorGlobally(offset);
 		
-		// Lerp it
-		double dX = driveable.posX + cameraPosition.x - posX;
-		double dY = driveable.posY + cameraPosition.y - posY;
-		double dZ = driveable.posZ + cameraPosition.z - posZ;
+		//Scaling the offset so that driveables going faster will be zoomed out.
+		//Scaling to the sqrt of the speed so the effect never gets too insane.
+		//Driveables moving at 50 m/s (Elytra + rocket is ~30 m/s) will be zoomed out 70% compared to normal
+		desiredPosition.scale((float) (Math.sqrt(driveable.getSpeed()) * 0.1 + 1));
 		
-		float lerpAmount = 0.1F;
+		Vector3f.add(desiredPosition, new Vector3f(driveable.posX, driveable.posY, driveable.posZ), desiredPosition);
+
+		float lerpAlpha = 0.25F;
 		
-		setPosition(posX + dX * lerpAmount, posY + dY * lerpAmount, posZ + dZ * lerpAmount);
+		//Lerping between desired position and old position
+		double newX = (1 - lerpAlpha) * posX + lerpAlpha * desiredPosition.x;
+		double newY = (1 - lerpAlpha) * posY + lerpAlpha * desiredPosition.y;
+		double newZ = (1 - lerpAlpha) * posZ + lerpAlpha * desiredPosition.z;
+		
+		
+		setPosition(newX, newY, newZ);
 		
 		rotationYaw = driveable.axes.getYaw() - 90;
 		rotationPitch = driveable.axes.getPitch();
 		
+		//Angle wrapping
 		while(rotationYaw - prevRotationYaw >= 180F)
 		{
 			rotationYaw -= 360F;
